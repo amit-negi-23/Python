69b448596c5953a87b12dcb71cd92143d576e3c4
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-13 19:23:21 +0530

    arithmetic operator

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,15 +1,11 @@
-#Data Types is Python
-"""
-1) Integers   +ve, -ve , 0
-2) String      
-3) Float        2.85
-4) Boolean      True , False
-5) None
+#Arithmetic operators
 
-"""
+a = 5
+b = 2
 
-#Single line comment
-
-"""
-This is multi-line comment
-"""
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
