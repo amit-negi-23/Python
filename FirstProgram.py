6d2fd33f51ec41d4b45638d8a177ca87b0c66d0b
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-13 20:30:48 +0530

    practice question

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,4 +1,27 @@
-#How to take input
+#Practice Question:- Write a program to input 2 numbers & print their sum.
 
-name = input("Enter Your name: ")  #return value is string
-print(type(name), name)
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum = num1 + num2
+print("sum =", sum)
+
+#Practice Question : - Write a program to input side of a square & print its area
+
+side = float(input("Enter side of square: "))
+print("area of square =", side*side)
+
+#Practice Question : - Write a program to input 2 floating point numbers and print their average
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("average =", (a + b)/2)
+
+#Practice Question : - Write a program to input 2 int numbers , a and b .
+#Print True if a is greater than or equal to b. If not print false
+
+x = int(input("Enter first integer: "))
+y = int(input("Enter second integer: "))
+
+print(x >= y)
