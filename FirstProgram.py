7375cb43ea04865f005ce53961dd35e8e8744167
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-13 18:13:55 +0530

    data types

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,1 +1,17 @@
-print("Hello World")
+#Data Types is Python
+"""
+1) Integers   +ve, -ve , 0
+2) String      
+3) Float        2.85
+4) Boolean      True , False
+5) None
+
+"""
+
+name1 = "AN"
+name2 = 'AN'
+name3 = '''AN'''
+
+print(name1)
+print(name2)
+print(name3)
