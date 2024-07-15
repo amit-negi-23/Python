5d2a7e6eb6e3bfb49d17e5203873f5d685485a75
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-15 14:23:40 +0530

    slicing

--- String&CondStatement.py
+++ String&CondStatement.py
@@ -1,14 +1,8 @@
-#indexing 
+#Slicing 
 
-strr = "Learning Python "
+Str = "Hello Python"
 
-print(strr[0])  # L
-print(strr[1])  # e
-print(strr[2])  # a
-print(strr[3])  # r
-
-
-
-#String are immutable
-# strr[2] = "@"   This line will give error
-print(strr[2])
+print(Str[0:5])         # Hello
+print(Str[6:12])        # Python
+print(Str[6:len(Str)])  # Python
+print(Str[6:])          # Python
