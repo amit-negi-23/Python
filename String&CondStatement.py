f8804ad032f018a6791670106618fc032b56b7e0
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-17 12:14:09 +0530

    practice question based on if elif

--- String&CondStatement.py
+++ String&CondStatement.py
@@ -1,12 +1,25 @@
-# Conditional Statement 
+#Practice Question
 
-age = 17
+# WAP to check if a number enter by user is even or odd
 
-if(age>=18):
-    if(age>75):
-        print("Can not Drive now, you are above 75")
-    else:
-        print("Can Drive")
-        
+num = int(input("Enter a number : "))
+
+if(num%2==0):
+    print("Even number", num)
 else:
-    print("Can not drive, you'r below 18")
+    print("Odd number", num)
+    
+    
+    
+# WAP to find the greatest of 3 numbers entered by user.
+
+num1 = int(input("enter first number :"))
+num2 = int(input("enter second number :"))
+num3 = int(input("enter third number :"))
+
+if(num1>num2 and num1>num3):
+    print("first number is largest =",num1)
+elif(num2>num3):
+    print("second number is largest =",num2)
+else:
+    print("third number is largest =",num3)
