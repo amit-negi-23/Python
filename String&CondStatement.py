d2abe775d6ff718c6342a102ef88d59ac2555d78
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-17 12:20:45 +0530

    practice question : greatest og 4 num

--- String&CondStatement.py
+++ String&CondStatement.py
@@ -1,25 +1,18 @@
 #Practice Question
-
-# WAP to check if a number enter by user is even or odd
-
-num = int(input("Enter a number : "))
-
-if(num%2==0):
-    print("Even number", num)
-else:
-    print("Odd number", num)
     
-    
-    
-# WAP to find the greatest of 3 numbers entered by user.
+# WAP to find the greatest of 4 numbers entered by user.
 
 num1 = int(input("enter first number :"))
 num2 = int(input("enter second number :"))
 num3 = int(input("enter third number :"))
+num4 = int(input("enter fourth number :"))
 
-if(num1>num2 and num1>num3):
+if(num1>num2 and num1>num3 and num1>num4):
     print("first number is largest =",num1)
-elif(num2>num3):
+elif(num2>num3 and num2>num4):
     print("second number is largest =",num2)
-else:
+elif(num3>num4):
     print("third number is largest =",num3)
+else:
+    print("fourth number is largest =",num4)
+    
