79ebca9765c49abfee945cd7991d2dff5f820ba8
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-17 10:57:43 +0530

    string function

--- String&CondStatement.py
+++ String&CondStatement.py
@@ -1,13 +1,26 @@
-#Slicing 
-
-Str = "Hello Python"
-
-print(Str[0:5])         # Hello
-print(Str[6:12])        # Python
-print(Str[6:len(Str)])  # Python
-print(Str[6:])          # Python
+msg = "i am learning Python myself"
+print(msg)
+print(len(msg))
 
 
-#Negative index
+#String Functions
 
-print(Str[-6:-2])       #Pyth
+# 1  msg.endswith("elf")     # return True or False
+print(msg.endswith("elf"))  #True
+print(msg.endswith("amit")) #False
+
+
+# 2  msg.capitalize()       # capitalize  1st char
+print(msg.capitalize())
+
+
+# 3  msg.replace("old", "new")     # replace all occurrences of old
+print(msg.replace("Python", "React"))
+
+
+# 4  msg.find(word)    # return 1st index of 1st occurrer
+print(msg.find("a"))
+              
+
+# 5  msg.count()       # count the occurrences of substr
+print(msg.count("n"))
