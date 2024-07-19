9e9703c269c10d83f56ba7827394ddcc18e9c341
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-19 16:31:06 +0530

    Tuple

--- ListAndTuple.py
+++ ListAndTuple.py
@@ -1,42 +1,6 @@
-# List Method
+# Tuple 
 
-num = [2, 1, 3]
-
-print(num)
-
-# lis.append()
-num.append(4)
-print(num)
-
-# lis.sort()
-num.sort()
-print(num)
-
-# lis.sort(reverse = True)
-num.sort(reverse=True)
-print(num)
-
-
-
-alpha = ['a', 'b', 'c', 'd', 'e', 'f']
-
-# lis.reverse()
-alpha.reverse()
-print(alpha)
-
-
-
-
-digi = [2, 1, 3, 1]
-
-# lis.insert(idx, el)
-digi.insert(1, 5)
-print(digi)
-
-# lis.remove(el)
-digi.remove(1)
-print(digi)
-
-# lis.pop(idx)
-digi.pop(0)
-print(digi)
+tup = (5, 4, 3, 2, 1)
+print(tup)
+print(type(tup))
+print(len(tup))
