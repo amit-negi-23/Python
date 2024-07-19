894ee6d6e26ea4b0667894c2f924c0e944486974
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-19 14:02:32 +0530

    list slicing

--- ListAndTuple.py
+++ ListAndTuple.py
@@ -1,10 +1,10 @@
-# List in Python
+# List Slicing
 
 marks = [94, 85, 82, 56, 64]
 
-print(marks)
-print(type(marks))
-print(len(marks))
 
-print(marks[0])
-print(marks[1])
+print(marks[0:3])
+print(marks[:3])
+print(marks[1:5])
+print(marks[1:])
+print(marks[1:len(marks)])
