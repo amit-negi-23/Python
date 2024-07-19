0318191e319f34f52ee619ed6a22d66f85cf1b81
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-19 23:22:17 +0530

    PQ 1 and 2

--- ListAndTuple.py
+++ ListAndTuple.py
@@ -1,6 +1,27 @@
-# Tuple Method
+# Practice Question 
 
-tup = (2, 1, 3, 1)
+# Write a program to ask the user to enter names of their 3 favorite movies & store them in a list.
 
-print(tup.index(1))  # return index of first occurrence
-print(tup.count(1))  # count total occurrence
+movies = []
+movies.append(input("Enter 1st movie name :"))
+movies.append(input("Enter 2nd movie name :"))
+movies.append(input("Enter 3rd movie name :"))
+
+print(movies)
+
+
+# WAP to check if a list contains a palindrome of elements. (Hint: use copy() method)
+
+list1 = [1, 2, 3, 2,1]
+
+copy_list1 = list1.copy()
+copy_list1.reverse()
+
+if(copy_list1 == list1):  # Here == is comparing the inner value only , not comparing the address ref 
+    print("palidrome")
+else:
+    print("NOT palidrome")
+    
+    
+# Note : if you want to compare the address ref use  ( is )
+print(copy_list1 is list1)
