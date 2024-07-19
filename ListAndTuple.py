a0bf87a9edc3e1a06e464b136794da1f1b3c2942
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-19 13:56:55 +0530

    list in python

---
+++ ListAndTuple.py
@@ -0,0 +1,10 @@
+# List in Python
+
+marks = [94, 85, 82, 56, 64]
+
+print(marks)
+print(type(marks))
+print(len(marks))
+
+print(marks[0])
+print(marks[1])
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Python
 Learning Python
+
+Day 1 : FirstProgram.py
+Day 2 : String&CondStatement.py
+Day 3 : 
