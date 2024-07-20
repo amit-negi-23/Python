dfc38ccae34f763e74787e228d8ba9516ca87107
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-20 12:31:02 +0530

    dictionary method

--- DictionaryAndSet.py
+++ DictionaryAndSet.py
@@ -1,4 +1,4 @@
-# Nested dictionary 
+# Dictionary Method
 
 student = {
     "name": "Amit Negi",
@@ -9,6 +9,30 @@ student = {
     }
 }
 
-print(student["subject"])
-print(student["subject"]["math"])
+
+# myDict.keys()       # return all keys
+print(student.keys())
+
+
+# myDict.values()      # return all values
+print(student.values())
+
+
+# myDict.items()       # return all (key, val) pair as tuples
+print(student.items())
+
+
+# myDict.get()          # return the value , and did not gives error if the key is not present
+print(student.get("name"))  # return value
+print(student.get("name1")) # return None
+
+
+# myDict.update(newDict)        # insert the specified items to the 
+student.update({"city": "Delhi"})
+print(student)
+
+
+newDict = {"age": "23", "email": "amit@gmail.com"}
+student.update(newDict)
+print(student)
 
