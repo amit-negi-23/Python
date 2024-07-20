4a3b0656dd0f717da9e8a9f69f1ae32ba0ec4d4a
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-20 13:30:47 +0530

    set union and intersection

--- DictionaryAndSet.py
+++ DictionaryAndSet.py
@@ -1,32 +1,11 @@
-# Set Method
-collection = set()
-
-# set.add(el)
-collection.add(1)
-collection.add(2)
-collection.add(3)
-collection.add("radha") #string added
-collection.add((79, 89, 99)) #tuple added
-#collection.add([1,2])  # can not add list
-
-print(collection)
+# set.union()
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+set_union = set1.union(set2)
+print(set_union)
 
 
 
-# set.remove(el)
-collection.remove(2) 
-print(collection)
-
-
-# set.clear()
-print("collection len", len(collection))
-collection.clear()
-print("collection len", len(collection))
-
-
-# set.pop()
-
-words = { "a", "b", "c", "d"}
-print(words)
-print("pop val :",words.pop())
-print(words)
+# set.intersection()
+set_intersec = set1.intersection(set2)
+print(set_intersec)
