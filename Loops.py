2e7c47d79cfe0079014eef452e292a8971c01d1f
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-20 18:51:01 +0530

    Practice Question 1 and 2

--- Loops.py
+++ Loops.py
@@ -1,10 +1,15 @@
-# while loop
+# while loop Practice Question
 
-# print number from 5 to 1
-
-i = 5
-while i >=1:
+# PQ 1: Print number from 1 to 100
+i = 1
+while i <= 100:
     print(i)
-    i-=1
-    
-print("Loop end")
+    i+=1
+print("Printed 1 to 100 : code end")
+
+# PQ 2: Print number from 100 to 1
+j = 100
+while j >= 1:
+    print(j)
+    j-=1
+print("Printed 100 to 1 : code end")
