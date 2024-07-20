31efe192c5fea0a82de864e0f471c6e3f65f909c
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-20 13:21:41 +0530

    set methods

--- DictionaryAndSet.py
+++ DictionaryAndSet.py
@@ -1,7 +1,32 @@
-# Set
+# Set Method
+collection = set()
+
+# set.add(el)
+collection.add(1)
+collection.add(2)
+collection.add(3)
+collection.add("radha") #string added
+collection.add((79, 89, 99)) #tuple added
+#collection.add([1,2])  # can not add list
+
+print(collection)
 
 
-X = set() # Empty Set
 
-print(type(X))
-print(len(X))
+# set.remove(el)
+collection.remove(2) 
+print(collection)
+
+
+# set.clear()
+print("collection len", len(collection))
+collection.clear()
+print("collection len", len(collection))
+
+
+# set.pop()
+
+words = { "a", "b", "c", "d"}
+print(words)
+print("pop val :",words.pop())
+print(words)
