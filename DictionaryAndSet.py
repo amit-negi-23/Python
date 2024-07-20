da63fe826b2c1fdbdf4154a77505384071de68bb
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-20 12:51:49 +0530

    empty set

--- DictionaryAndSet.py
+++ DictionaryAndSet.py
@@ -1,12 +1,7 @@
 # Set
 
-X = {1, 2, 3, 2, 4, 1}
-print(X)
-print(len(X))
+
+X = set() # Empty Set
+
 print(type(X))
-
-
-
-Y = { 1, 2, 3, "aman", "radha","aman", "radha", "rohit"}
-print(Y)
-print(len(Y))
+print(len(X))
