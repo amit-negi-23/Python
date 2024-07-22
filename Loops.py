4e9767023916d9dacbd3ca70149644f9a6de2871
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-22 11:42:17 +0530

    Practice Question

--- Loops.py
+++ Loops.py
@@ -1,14 +1,20 @@
-# range()
+# Practice Question 
 
-for i in range(5):  # range(stop)
-    print(i)
-    
-print("*****************")
+# PQ 1: Print number from 1 to 100
 
-for i in range(2, 5):  #range(start, stop)
+for i in range(1, 101):
     print(i)
 
-print("*****************")   
 
-for i in range(2, 10, 2): # range(start, stop, step)
+# PQ 2: Print number from 100 to 1
+
+for i in range(100, 0 , -1):
     print(i)
+
+
+# PQ 3: Print the multiplication tabel of a number n.
+
+num = int(input("Enter the number :"))
+
+for i in range(1, 11):
+    print(num*i)
