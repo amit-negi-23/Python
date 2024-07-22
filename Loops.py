6fbc66c76c819f2512c3989a7db2cb15b5064296
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-22 11:30:30 +0530

    range(start, stop, step)

--- Loops.py
+++ Loops.py
@@ -1,18 +1,14 @@
-# Practice Question
+# range()
 
-# PQ 1:
-nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+for i in range(5):  # range(stop)
+    print(i)
+    
+print("*****************")
 
-for el in nums:
-    print(el)
-    
-    
-# PQ 2:
-digit = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 49)
-x = 49
-idx =0
-for el in digit:
-    if(el ==x):
-        print("number found at idx", idx)
-        break
-    idx+=1
+for i in range(2, 5):  #range(start, stop)
+    print(i)
+
+print("*****************")   
+
+for i in range(2, 10, 2): # range(start, stop, step)
+    print(i)
