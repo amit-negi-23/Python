ef6712043f8089cecb2ff8b0260bd2f1f0feb2f4
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-22 10:50:33 +0530

    else in loop

--- Loops.py
+++ Loops.py
@@ -4,15 +4,5 @@ nums = [1, 2, 3, 4, 5]
 
 for val in nums:
     print(val)
-    
-
-fruits = ("apple", "banana", "papaya", "mango","strawberry")
-
-for val in fruits:
-    print(val)
-
-
-name = "AmitNegi"
-
-for val in name:
-    print(val)
+else:
+    print("End loop")
