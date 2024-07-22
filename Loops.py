d088c98dce33865b3de6ce3c8e6b12f2897d9195
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-22 12:44:24 +0530

    pass

--- Loops.py
+++ Loops.py
@@ -1,20 +1,6 @@
-# Practice Question 
+# pass 
 
-# PQ 1: Print number from 1 to 100
-
-for i in range(1, 101):
-    print(i)
-
-
-# PQ 2: Print number from 100 to 1
-
-for i in range(100, 0 , -1):
-    print(i)
-
-
-# PQ 3: Print the multiplication tabel of a number n.
-
-num = int(input("Enter the number :"))
-
-for i in range(1, 11):
-    print(num*i)
+for i in range(5):
+    pass
+    
+print("code ends")
