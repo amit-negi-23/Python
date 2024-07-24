7abb08d12f1075178d47cc1952c3bf01b6407a84
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-24 11:48:48 +0530

    PQ 1 and 2

--- function.py
+++ function.py
@@ -1,6 +1,21 @@
-print("Hello","Amit", sep="-->", ) # sep = " " (default)
+#practice question
+
+# PQ 1: WAF to print the length of a list (list is the parameter)
+cities = ["delhi", "mumbai", "hyderabad", "chennai", "koltata", "bengaluru", "noida ", "gurgaon"]
+
+fruits = ["Custard Apple", "Pomegranate", "Orange", "Papaya", "Strawberry"]
+
+def print_len(list):
+    print(len(list))
+
+print_len(cities)
+print_len(fruits)
 
 
+# PQ 2: WAF to print the elements of a list in a single line. (list is the parameter)
 
-print("Python",end="***")   # end= "\n"  (default)
-print("Javascript")
+def print_list(list):
+    for item in list:
+        print(item, end=", ")
+
+print_list(cities)
