27a82b56080f7d21d79e8a93900e47b9fd63e4b5
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-25 23:16:07 +0530

    PQ : Account / debit/ credit / check balance

--- Oops_One.py
+++ Oops_One.py
@@ -1,14 +1,26 @@
-# Abstraction : Hiding  unnecessary information and display only necessary information to the users interacting. 
+# Practice Question
 
-                #or
-                
-# Hiding the implementation complexity and details from the user.
+class Account:
+    def __init__(self, bal, acc):
+        self.balance = bal
+        self.account_no = acc
+
+    def debit(self, debit_amt):
+        self.balance-=debit_amt
+        print("Rs.", debit_amt, "was debited")
+        print("total balance =", self.get_balance())
+
+    def credit(self, credit_amt):
+        self.balance+=credit_amt
+        print("Rs.", credit_amt, "was credited")
+        print("total balance =", self.get_balance())
+    
+    def get_balance(self):
+        return self.balance
 
 
+acc1 = Account(5000, 59085591419)
+acc1.debit(500)
+acc1.credit(1000)
 
-
-
-
-# Encapsulation : Wrapping data and function into a single unit . 
-
-
+print(acc1.get_balance())
