2f5dc7806b8f64a3ffd3634e3829cfd23d13e247
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-25 19:41:15 +0530

    find line in which word exist

--- I_O_inFile.py
+++ I_O_inFile.py
@@ -1,10 +1,17 @@
-# PQ 3
-word = "learning"
-with open("practice.txt","r") as file:
-    data = file.read()
-    if(data.find(word) != -1):
-        print("Found")
-    else:
-        print("not found")
-        
-    file.close()   
+# PQ 4:
+
+def check_for_line():
+    word = "learning"
+    data = True
+    line_no = 1
+    with open("practice.txt", "r") as f:
+        while data:
+            data = f.readline()
+            if(word in data):
+                print(line_no)
+                return
+            line_no+=1
+
+    return -1
+
+check_for_line()
