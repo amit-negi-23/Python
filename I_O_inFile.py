d5e53024b5f8b78edb6f4275bf4e3314183e66d8
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-25 19:03:05 +0530

    PQ 1

--- I_O_inFile.py
+++ I_O_inFile.py
@@ -1,3 +1,5 @@
-import os
-
-os.remove("test.py")
+#Pq 1: 
+with open("practice.txt", "w") as file:
+    file.write("Hi everyone\nwe are learning File I/O\n")
+    file.write("using Java.\nI like programming in Java.")
+    file.close()
