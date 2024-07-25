2db951386aba82433833cca13ad0585b3cf31691
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-25 22:54:34 +0530

    Abstraction and Encapsulation

--- Oops_One.py
+++ Oops_One.py
@@ -1,27 +1,14 @@
-# practice Question
+# Abstraction : Hiding  unnecessary information and display only necessary information to the users interacting. 
 
-class Student:
-    def __init__(self, name, marks):
-        self.name = name
-        self.marks = marks
-
-    @staticmethod
-    def hello():
-        print("hello")
-
-    def get_avg(self):
-        sum =0
-        for val in self.marks:
-            sum+=val
-        print("Hi",self.name, "Your avg score is :", sum/3)
+                #or
+                
+# Hiding the implementation complexity and details from the user.
 
 
-s1 = Student("Amit Negi", [97, 92, 99])
-s1.get_avg()
 
 
-s1.hello()
 
-# Update
-s1.name="Rohit"
-s1.get_avg()
+
+# Encapsulation : Wrapping data and function into a single unit . 
+
+
