725f7a97872b39555a89a995dbde1ab29651ac5a
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-26 14:10:28 +0530

    polymorphism

--- Oops_Two.py
+++ Oops_Two.py
@@ -1,15 +1,24 @@
-class Student:
-    def __init__(self, phy, chem, math):
-        self.phy = phy
-        self.chem = chem
-        self.math = math
-       
-    @property
-    def percentage(self):
-        return str((self.phy + self.chem + self.math)/3) + "%"
+# Polymorphism : 
 
-stu1 = Student(98, 97, 99)
-print(stu1.percentage)
+class Complex:
+    def __init__(self, real, img):
+        self.real = real
+        self.img = img
+        
+    def showNumber(self):
+        print(self.real,"i +",self.img, "j")
+        
+    def __add__(self, newNum):
+        newReal = self.real + newNum.real
+        newImg = self.img + newNum.img
+        return Complex(newReal, newImg)
+    
+    
+num1 = Complex(2, 3)
+num1.showNumber()
 
-stu1.phy = 86
-print(stu1.percentage)
+num2 = Complex(4, 5)
+num2.showNumber()
+
+num3 = num1 + num2
+num3.showNumber()
