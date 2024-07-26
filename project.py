b67d14e6986cde62aeb3b26654a05a8dd6ad98e4
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-26 21:57:37 +0530

    Random password : list comprehension

--- project.py
+++ project.py
@@ -4,8 +4,13 @@ import string
 pass_len = 8
 charValues = string.ascii_letters + string.digits + string.punctuation
 
-password = ""
-for i in range(pass_len):
-    password += random.choice(charValues)
+# list comprehension [function for i in range(n)]
+
+password = "".join([random.choice(charValues) for i in range(pass_len)])
+
+
+# password = ""
+# for i in range(pass_len):
+#     password += random.choice(charValues)
 
 print("your random password is:", password)
