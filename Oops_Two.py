84698254e7f28c356a0d8c6acbd0f5251976dc51
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-26 13:34:54 +0530

    @property

--- Oops_Two.py
+++ Oops_Two.py
@@ -3,16 +3,13 @@ class Student:
         self.phy = phy
         self.chem = chem
         self.math = math
-        # self.percentage = str((self.phy + self.chem + self.math)/3) + "%"
-
-    # def calcPercentage(self):
-    #     self.percentage = str((self.phy + self.chem + self.math)/3) + "%"
+       
     @property
     def percentage(self):
         return str((self.phy + self.chem + self.math)/3) + "%"
 
 stu1 = Student(98, 97, 99)
 print(stu1.percentage)
+
 stu1.phy = 86
-# stu1.calcPercentage()
 print(stu1.percentage)
