4dac6d43a06b80a6079a65bdc4c66eefb97dce9a
amit-negi-23 <amitnegiit23@gmail.com>
2024-07-26 21:14:01 +0530

    pQ 3

--- Oops_Two.py
+++ Oops_Two.py
@@ -1,43 +1,14 @@
-# Practice Question
+# PQ 3:
 
-# PQ 1:
-class Circle:
-    def __init__(self, radius):
-        self.radius = radius
+class Order:
+    def __init__(self, item, price):
+        self.item = item
+        self.price = price
         
-    def Area(self):
-        return 3.14 * (self.radius)**2
+    def __gt__(self, odr2):
+        return self.price > odr2.price
+        
+odr1 = Order("momos", 80)
+odr2 = Order("chips", 20)
 
-    def Perimeter(self):
-        return 2 * 3.14 * self.radius
-
-
-c1 = Circle(7)
-print(c1.Area())
-print(c1.Perimeter())
-
-
-# PQ 2:
-
-class Employee:
-    def __init__(self, role, dept, salary):
-        self.role = role
-        self.dept = dept
-        self.salary = salary
-
-    def showDetails(self):
-        print("role =", self.role)
-        print("dept =", self.dept)
-        print("salary =", self.salary)
-
-
-class Engineer(Employee):
-    def __init__(self, name, age):
-        super().__init__("Engineer", "IT", "75,000")
-        self.name = name
-        self.age = age
-
-
-engg1 = Engineer("Rohit", 25)
-print(engg1.name)
-engg1.showDetails()
+print(odr1>odr2)  #true
